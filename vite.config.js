333d4f35a9fdc650f87ecccc5efbf2b398b3a501
Omar Vite <omar.vite@culqi.com>
2025-11-16 19:22:41 -0500

    feat: envs for production

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import federation from '@originjs/vite-plugin-federation'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    federation({
-      name: 'host-app',
-      remotes: {
-        mf1: 'http://localhost:5001/assets/remoteEntry.js',
-        mf2: 'http://localhost:5002/assets/remoteEntry.js',
-      },
-      shared: ['vue']
-    })
-  ],
-  build: {
-    target: 'esnext',
-    minify: false,
-    cssCodeSplit: false
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [
+      vue(),
+      federation({
+        name: 'host-app',
+        remotes: {
+          mf1: env.VITE_MFE1_URL || 'http://localhost:5001/assets/remoteEntry.js',
+          mf2: env.VITE_MFE2_URL || 'http://localhost:5002/assets/remoteEntry.js',
+        },
+        shared: ['vue']
+      })
+    ],
+    build: {
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false
+    }
   }
 })
