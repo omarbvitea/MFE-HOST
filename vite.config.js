9ab4b0b1522284e80e5fe08e7cb439d3b03df27c
Omar Vite <omar.vite@culqi.com>
2025-11-16 19:51:42 -0500

    feat: gh pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
+    base: env.VITE_BASE_URL || '/',
     plugins: [
       vue(),
       federation({
